3f4219cf5d1ec8a3f6e705ef2bde72413235c722
mymriffer-eng <mymriffer@gmail.com>
2026-01-09 11:15:38 +0200

    Switch from mysqlclient to PyMySQL for easier installation

--- products/settings.py
+++ products/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from decouple import config, Csv
 import os
+import pymysql
+
+# PyMySQL compatibility for Django
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
