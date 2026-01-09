dd34ecdbc011ff0a861bad7d8bfd70d3dc3d91a8
mymriffer-eng <mymriffer@gmail.com>
2026-01-09 16:41:32 +0200

    Fix media files serving in production using re_path

--- products/urls.py
+++ products/urls.py
@@ -15,19 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from decouple import config
 
 urlpatterns = [
     path(config('ADMIN_URL', default='admin/'), admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('catalog.urls')),
+    # Serve media files in production
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# Serve media files in both development and production
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
