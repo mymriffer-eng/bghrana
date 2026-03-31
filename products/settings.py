055f970c1436c9f0602808bcf8ca112cb1e87d24
mymriffer-eng <mymriffer@gmail.com>
2026-04-01 01:22:15 +0300

    Добавен Facebook provider в INSTALLED_APPS

--- products/settings.py
+++ products/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
 ]
 
 # Django Debug Toolbar (само в DEBUG mode)
